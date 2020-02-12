c42bb89d722f7fdb3aed339be919bff7a211972d
MaciejMatyaszek <maciej.matyaszek10@gmail.com>
2020-02-12 13:48:04 +0100

    Initial commit

---
+++ src/main/java/com/xavier/Elenx/ElenxApplication.java
@@ -0,0 +1,13 @@
+package com.xavier.Elenx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElenxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElenxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xavier/Elenx/ElenxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavier.Elenx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElenxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
